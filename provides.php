5b5a02c4893d715004811df62f75cbf92f1a04e0
Eric Holtam <Eric.Holtam@meredith.com>
2017-03-17 10:39:26 -0500

    updated provides.php with name change

--- provides.php
+++ provides.php
@@ -2,8 +2,6 @@
 
 return array(
     'listings' => array(
-        'johnslist' => array('view' => 'johnslist', 'i18n' => 'johnslist.johnslist',),
+        'johnslist' => array('view' => 'johnslist', 'i18n' => 'johnslist.johnslist'),
     ),
 );
-
-
