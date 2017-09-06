a60d6a951fd3454bebb282b120e37d9531205040
Eric Holtam <Eric.Holtam@meredith.com>
2017-09-06 10:58:10 -0500

    Removed media due to duplicates

--- views/johnslist.php
+++ views/johnslist.php
@@ -30,8 +30,6 @@ new department_model;
 			<th data-i18n="listing.department.department" data-colname='department.department'>Department</th>
 			<th data-colname='reportdata.timestamp'>Check-in</th>
 			<th data-i18n="serial" data-colname='reportdata.serial_number'>Serial</th>
-			<th data-i18n="disk_report.media_type" data-colname='diskreport.VolumeType'>Media Type</th>
-		    <th data-i18n="disk_report.mountpoint" data-colname='diskreport.MountPoint'>Mount Point</th>
 		  </tr>
 		</thead>
 
